05c9fc16d32fc48d9d60e4d310cf61bf9b629b46
Jhon Jaime Macias Quezada <jhon.macias.dev@gmail.com>
2025-07-11 10:15:25 +0200

    Fix: correccion numero

--- app/components/CallButton.tsx
+++ app/components/CallButton.tsx
@@ -7,7 +7,7 @@ export default function BotonLlamar() {
 
   return (
     <a
-      href="tel:633662935"
+      href="tel:633693246"
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
       className="px-6 py-3 bg-[#FD5314] rounded text-xl font-semibold hover:bg-[#FF7F3F] transition duration-300 ease-in-out transform hover:scale-105 inline-block"
